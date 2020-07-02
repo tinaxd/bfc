4824194dd87a6baedd603f80d9e6a913015fa618
tinaxd <31384595+tinaxd@users.noreply.github.com>
2020-07-03 00:31:37 +0900

    hobo complete

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <stack>
 #include <stdexcept>
+#include <optional>
+#include <fstream>
+#include <sstream>
 
 enum Instruction
 {
@@ -78,6 +81,7 @@ buildProgram(const std::vector<Instruction> &insts)
             size_t jmpto = loopstack.top();
             loopstack.pop();
             cmds.emplace_back(inst, jmpto);
+            cmds.at(jmpto).jumpTo = i;
             break;
         }
         case LOOP:
@@ -93,65 +97,186 @@ buildProgram(const std::vector<Instruction> &insts)
     return cmds;
 }
 
-std::string asm_right() { return "incr r9"; }
-std::string asm_left() { return "decr r9"; }
-std::string asm_incr() {}
-std::string asm_decr() {}
-std::string asm_put() {}
-std::string asm_get() {}
-std::string asm_loop(const std::string &label) {}
-std::string asm_jmp(const std::string &label) {}
+std::string asm_right() { return "inc r8"; }
+std::string asm_left() { return "dec r8"; }
+std::string asm_incr() { return "inc byte [rsp+r8]"; }
+std::string asm_decr() { return "dec byte [rsp+r8]"; }
+std::vector<std::string> asm_put()
+{
+    return {
+        "mov rax, 1",
+        "mov rdi, 1",
+        "mov rsi, buf",
+        "mov rdx, 1",
+        "mov r9b, byte [rsp+r8]",
+        "mov byte [buf], r9b",
+        "syscall"};
+}
+std::vector<std::string> asm_get()
+{
+    return {
+        "mov rax, 0",
+        "mov rdi, 0",
+        "mov rsi, buf",
+        "mov rdx, 1",
+        "syscall",
+        "mov r9b, [buf]",
+        "mov byte [rsp+r8], r9b"};
+}
+std::vector<std::string> asm_loop(const std::string &label, const std::string &jmpTo)
+{
+    return {
+        label + ":",
+        "mov r10b, byte [rsp+r8]",
+        "test r10b, r10b",
+        "jz " + jmpTo};
+}
+std::string asm_jmp(const std::string &label) { return "jmp " + label; }
 
 std::string int_to_label(int n)
 {
     return "LP" + std::to_string(n);
 }
 
+std::vector<std::string> asm_init()
+{
+    return {
+        "global _start",
+        "section .data",
+        "buf: db 0",
+        "section .text",
+        "_start:",
+        "xor r8, r8",
+        "sub rsp, 30000"};
+}
+
+std::vector<std::string> asm_tail()
+{
+    return {
+        "add rsp, 30000",
+        "mov rax, 60",
+        "xor rdi, rdi",
+        "syscall"};
+}
+
+template <typename T>
+void extend(std::vector<T> &vec, const std::vector<T> &ext)
+{
+    for (const auto &elem : ext)
+    {
+        vec.push_back(elem);
+    }
+}
+
 std::vector<std::string> assembly(const std::vector<Command> &cmds)
 {
-    std::vector<std::string> asms;
+    std::vector<std::string> asms = asm_init();
     for (size_t i = 0; i < cmds.size(); i++)
     {
         const auto &cmd = cmds.at(i);
         switch (cmd.inst)
         {
         case RIGHT:
-            asm_right();
+            asms.push_back(asm_right());
             break;
         case LEFT:
-            asm_left();
+            asms.push_back(asm_left());
             break;
         case PLUS:
-            asm_incr();
+            asms.push_back(asm_incr());
             break;
         case MINUS:
-            asm_decr();
+            asms.push_back(asm_decr());
             break;
         case PUT:
-            asm_put();
+            extend(asms, asm_put());
             break;
         case GET:
-            asm_get();
+            extend(asms, asm_get());
             break;
         case LOOP:
-            asm_loop(int_to_label(i));
+            extend(asms, asm_loop(
+                             int_to_label(i),
+                             int_to_label(cmd.jumpTo)));
             break;
         case JMP:
-            asm_jmp(int_to_label(cmd.jumpTo));
+            asms.push_back(asm_jmp(int_to_label(cmd.jumpTo)));
             break;
         }
     }
+    extend(asms, asm_tail());
     return asms;
+}
+
+std::optional<Instruction> readChar(char c)
+{
+    switch (c)
+    {
+    case '+':
+        return std::make_optional(PLUS);
+    case '-':
+        return std::make_optional(MINUS);
+    case '>':
+        return std::make_optional(RIGHT);
+    case '<':
+        return std::make_optional(LEFT);
+    case '.':
+        return std::make_optional(PUT);
+    case ',':
+        return std::make_optional(GET);
+    case '[':
+        return std::make_optional(LOOP);
+    case ']':
+        return std::make_optional(JMP);
+    }
+    return std::optional<Instruction>();
+}
+
+std::vector<Instruction> readInstructions(const std::string &text)
+{
+    std::vector<Instruction> insts;
+    for (const char c : text)
+    {
+        auto i = readChar(c);
+        if (i.has_value())
+        {
+            insts.push_back(std::move(i.value()));
+        }
+    }
+    return insts;
 }
 
 int main(int argc, char **argv)
 {
-    std::vector<Instruction> insts{PLUS, PLUS, LOOP, PLUS, MINUS, JMP, RIGHT};
+    if (argc != 2)
+    {
+        std::cerr << "usage: bfc <filename>" << std::endl;
+        return 2;
+    }
+
+    std::ifstream in(argv[1]);
+    if (!in.is_open())
+    {
+        std::cerr << "could not read " << argv[1] << std::endl;
+        return 1;
+    }
+    std::stringstream buffer;
+    buffer << in.rdbuf();
+    const auto programText = buffer.str();
+
+    const auto insts = readInstructions(programText);
     const auto program = buildProgram(insts);
     const auto asmcode = assembly(program);
+
+    std::ofstream out("out.asm");
+    if (!out.is_open()) {
+        std::cerr << "could not open out.asm" << std::endl;
+        return 1;
+    }
+
     for (const auto &sasm : asmcode)
     {
-        std::cout << sasm << std::endl;
+        out << sasm << std::endl;
     }
     return 0;
 }
